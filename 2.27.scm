13332a3a60fd0e1ad07bc134c627c71f2a9ac1a8
Wang Hsutung <xukiro@outlook.com>
2016-02-22 18:19:01 +0800

    2.27 completed.

---
+++ 2.27.scm
@@ -0,0 +1,12 @@
+(define nil '())
+
+(define (deep-reverse items)
+  (define (reverse-iter dest src)
+    (cond ((null? src) dest)
+          ((not (pair? src)) src)
+          (else (reverse-iter (cons (deep-reverse (car src)) dest)
+                              (cdr src)))))
+  (reverse-iter '() items))
+
+(deep-reverse (list 1 2 3 4))
+(deep-reverse '())
