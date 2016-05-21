f314fe27cb84dd0b089e76fd0da694020eae0c43
Wang Hsutung <xukiro@outlook.com>
2016-05-21 14:30:20 +0800

    2.68 增加测试代码

--- 2.68.scm
+++ 2.68.scm
@@ -1,5 +1,11 @@
 (load "huffman.scm")
+(load "2.67.scm")
 
+(define (encode message tree)
+  (if (null? message)
+    '()
+    (append (encode-symbol (car message) tree)
+            (encode (cdr message) tree))))
 
 (define (encode-symbol symbol tree)
   (if (leaf? tree)
@@ -9,7 +15,13 @@
              (l-symbols (symbols l-branch))
              (r-symbols (symbols r-branch)))
         (cond ((member symbol l-symbols)
-               (cons 0 (encode-symbol symbol l-branch)))
+               (cons 0
+                     (encode-symbol symbol l-branch)))
               ((member symbol r-symbols)
-               (cons 1 (encode-symbol symbol r-branch)))
-              (else (list "Error"))))))
+               (cons 1
+                     (encode-symbol symbol r-branch)))
+              (else (error "This is not included in set" symbol))))))
+
+(display sample-message)
+(newline)
+(display (encode (decode sample-message sample-tree) sample-tree))
