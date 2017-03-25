f65f88cb2bf00e825fc57484290bcd565f9545a9
Wang <wang@Yet-Another-MBP>
2017-03-25 12:48:39 +0800

    Stream 3.58

---
+++ 3.58.scm
@@ -0,0 +1,8 @@
+(load "inf-stream.scm")
+
+(define (expand num den radix)
+  (cons-stream
+    (quotient (* num radix) den)
+    (expand (remainder (* num radix) den)
+            den
+            radix)))
