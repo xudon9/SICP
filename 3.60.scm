511cd3f049f6508abea324f7bfdd6eb71f2e21da
Wang <wang@Yet-Another-MBP>
2017-03-29 01:58:30 +0800

    Stream 3.61

--- 3.60.scm
+++ 3.60.scm
@@ -1,11 +1,13 @@
 (load "3.59.scm")
 
+;;; 厉害了这个解法
 (define (mul-series s1 s2)
   (cons-stream (* (stream-car s1)
                   (stream-car s2))
                (add-streams (scale-stream (stream-cdr s1)
                                           (stream-car s2))
                             (mul-series s1 (stream-cdr s2)))))
+
 (define cos-square+sin-square
   (add-streams (mul-series cosine-series cosine-series)
                (mul-series sine-series sine-series)))
---
+++ 3.61.scm
@@ -0,0 +1,7 @@
+(load "3.60.scm")
+
+(define (invert-unit-series s)
+  (cons-stream 1
+               (scale-stream (mul-series (stream-cdr s)
+                                         (invert-unit-series s))
+                             -1)))
