ecd8d2a5fdb546d9204fd9946b686a782aa00428
Wang <wang@Yet-Another-MBP>
2020-09-16 23:05:23 +1000

    2020

---
+++ 3.69.scm
@@ -0,0 +1,21 @@
+(load "stream-process.scm")
+
+(define (pairs s t)
+  (cons-stream (list (stream-car s) (stream-car t))
+               (interleave (stream-map (lambda (x)
+                                         (list (stream-car s) x))
+                                       (stream-cdr t))
+                           (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+    s2
+    (cons-stream (stream-car s1)
+                 (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t u)
+  (cons-stream (list (stream-car s)
+                     (stream-car t)
+                     (stream-car u))
+               )
+  )
---
+++ ch4.scm
@@ -0,0 +1,36 @@
+;;;SECTION 4.1.1
+;;; **SEE ALSO** ch4-mceval.scm (loadable/runnable evaluator)
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp) 
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Unknown expression type -- EVAL" exp))))
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             (procedure-parameters procedure)
+             arguments
+             (procedure-environment procedure))))
+        (else
+         (error
+          "Unknown procedure type -- APPLY" procedure))))
